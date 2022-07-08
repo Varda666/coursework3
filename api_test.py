a365fab4e2c624f01e0128886ee9cea582630c5d
Varda666 <oksana.aeshina666@gmail.com>
2022-07-08 22:04:43 +0300

    task 5-8/3

--- api_test.py
+++ api_test.py
@@ -5,16 +5,16 @@ def test_all():
     response = app.test_client().get('/api/posts/')
     assert response.status_code == 200
     assert type(response.json) == list, "Возвращается не список"
-    keys = ["poster_avatar", "poster_name", "views_count", "content", 'likes_count', 'pic']
+    keys = ["poster_avatar", "poster_name", "views_count", "content", 'likes_count', 'pic', 'pk', 'c']
     for item in response.json:
         for key in item:
             assert key in keys, 'Такого ключа в словаре нет'
 
 def test_one():
-    response = app.test_client().get('/api/posts/<int:pk>')
+    response = app.test_client().get('/api/posts/2')
     assert response.status_code == 200
     assert type(response.json) == dict, "Возвращается не словарь"
-    keys = ["poster_avatar", "poster_name", "views_count", "content", 'likes_count', 'pic']
+    keys = ["poster_avatar", "poster_name", "views_count", "content", 'likes_count', 'pic', 'pk', 'c']
     for item in response.json:
         for key in item:
             assert key in keys, 'Такого ключа в словаре нет'
